bbe4732bfcbf06f16802266212ec32e3981b5626
ulwar <olszewski_marcin@wp.pl>
2020-05-18 17:36:45 +0200

    start

---
+++ src/test/java/amadeuszx/petclinic/springpetclinic/SpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package amadeuszx.petclinic.springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
