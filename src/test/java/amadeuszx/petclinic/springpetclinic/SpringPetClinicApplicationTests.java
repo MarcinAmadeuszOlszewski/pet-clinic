01b0afdcfd9da112029f36dafcd1e5f3433d8a05
ulwar <olszewski_marcin@wp.pl>
2020-05-18 17:58:37 +0200

    .

--- src/test/java/amadeuszx/petclinic/springpetclinic/SpringPetClinicApplicationTests.java
+++ src/test/java/amadeuszx/petclinic/springpetclinic/SpringPetClinicApplicationTests.java
@@ -1,13 +1,15 @@
 package amadeuszx.petclinic.springpetclinic;
 
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.testng.annotations.AfterTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringPetClinicApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
