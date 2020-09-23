1fe6da3f60ac18c2ad96d8633681699e8ececdff
Marcin Olszewski <marcin.olszewski@akmf.pl>
2020-09-23 15:10:40 +0200

    Refactor services interface

--- pet-clinic-data/src/main/java/amadeuszx/petclinic/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/amadeuszx/petclinic/springpetclinic/services/VetService.java
@@ -1,13 +1,6 @@
 package amadeuszx.petclinic.springpetclinic.services;
 
 import amadeuszx.petclinic.springpetclinic.model.Vet;
-import java.util.Set;
 
-public interface VetService {
-
-  Vet findById(Long id);
-
-  Vet save(Vet vet);
-
-  Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
