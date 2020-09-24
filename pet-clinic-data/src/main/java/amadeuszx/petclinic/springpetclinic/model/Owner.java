54692a88e3b3a0dce2c444040f62989a478e8209
Marcin Olszewski <marcin.olszewski@akmf.pl>
2020-09-24 16:30:14 +0200

    added Visit

--- pet-clinic-data/src/main/java/amadeuszx/petclinic/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/amadeuszx/petclinic/springpetclinic/model/Owner.java
@@ -1,4 +1,15 @@
 package amadeuszx.petclinic.springpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+  private Set<Pet>  pets;
+
+  public Set<Pet> getPets() {
+    return pets;
+  }
+
+  public void setPets(final Set<Pet> pets) {
+    this.pets = pets;
+  }
 }
