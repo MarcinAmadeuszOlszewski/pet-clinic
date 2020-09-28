e8dab9d0719cf577bd461c004d735fde7379acd3
Marcin Olszewski <marcin.olszewski@akmf.pl>
2020-09-28 20:47:06 +0200

    JPA map entities

--- pet-clinic-data/src/main/java/amadeuszx/petclinic/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/amadeuszx/petclinic/springpetclinic/model/Vet.java
@@ -2,9 +2,21 @@ package amadeuszx.petclinic.springpetclinic.model;
 
 import java.util.HashSet;
 import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "vetd")
 public class Vet extends Person {
 
+  @ManyToMany(fetch = FetchType.EAGER)
+  @JoinTable(name = "vets_specialties",
+      joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "specialties_id"))
   private Set<Speciality> specialities = new HashSet<>();
 
   public Set<Speciality> getSpecialities() {
