11699f473fef571418a20109956962422b515a82
Marcin Olszewski <marcin.olszewski@akmf.pl>
2020-09-28 20:34:32 +0200

    JPA map entities

--- pet-clinic-data/src/main/java/amadeuszx/petclinic/springpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/amadeuszx/petclinic/springpetclinic/model/PetType.java
@@ -1,7 +1,15 @@
 package amadeuszx.petclinic.springpetclinic.model;
 
-public class PetType extends BaseEntity{
-private String name;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
+public class PetType extends BaseEntity {
+
+  @Column(name = "name")
+  private String name;
 
   public String getName() {
     return name;
