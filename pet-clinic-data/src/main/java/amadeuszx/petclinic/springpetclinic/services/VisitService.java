6cde1e480f16fc44ede46d026bfd7cf08ea69742
ulwar <olszewski_marcin@wp.pl>
2021-01-31 18:28:53 +0100

    VisitService and its impl

---
+++ pet-clinic-data/src/main/java/amadeuszx/petclinic/springpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package amadeuszx.petclinic.springpetclinic.services;
+
+import amadeuszx.petclinic.springpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
